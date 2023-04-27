678a75814d8171d3a7483bfae50737985611eeb6
Kuschenko Marina <marinakuschenko1@yandex.ru>
2023-04-27 20:57:55 +0300

    ПР9

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -6,11 +6,11 @@ export default class Popup{
       }
     open(){
         this._popup.classList.add('popup_active');
-        document.addEventListener('keydown', this.handleEscClose);
+        document.addEventListener('keydown', this._handleEscClose);
     }
     close(){
         this._popup.classList.remove('popup_active');
-        document.removeEventListener('keydown', this.handleEscClose);
+        document.removeEventListener('keydown', this._handleEscClose);
     }
     _handleEscClose = (evt)=>{
         if (evt.key === 'Escape') {
